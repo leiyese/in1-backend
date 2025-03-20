8ca9cc4dd599cfbd51b138f8a6bd26f7f5e4511b
Anton Ernstsson <129973980+korv9@users.noreply.github.com>
2025-03-20 11:50:11 +0100

    Update User.py

--- models/User.py
+++ models/User.py
@@ -9,7 +9,7 @@ class User(db.Model):
     corporation_id = db.Column(db.Integer, db.ForeignKey('corporations.id'), nullable=False)
     username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     
     def __repr__(self):
         return f"<User {self.username}>"
