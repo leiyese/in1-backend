ca9e0b9832602a0e305d9acc42cf7d3660a67683
Cronan99 <albincronmark@gmail.com>
2025-03-21 17:20:40 +0100

    Added jwt_manager in app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from config.db import get_database_uri,db
 import os
@@ -12,6 +13,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = get_database_uri()
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+jwt = JWTManager(app)
 
 app.register_blueprint(auth_routes), url_prefix="/authenticate"
 
