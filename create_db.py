31892bbdb8001045bfe35ed3d5a2142b3e8d7f3b
Anton Ernstsson <129973980+korv9@users.noreply.github.com>
2025-03-25 13:43:53 +0100

    Update create_db.py with new models

--- create_db.py
+++ create_db.py
@@ -1,7 +1,10 @@
 from app import app, db
-from models.User import User
-from models.Corporation import Corporation
-from models.Prompthistory import Prompthistory
+from models.ai_models import AI_model, AI_type
+from models.user import User
+from models.subscription_models import Subscription, Subscription_type
+from models.payment_models import Payment
+from models.corporation import Corporation
+from models.prompthistory import Prompthistory
 # Import additional models as needed
 
 with app.app_context():
